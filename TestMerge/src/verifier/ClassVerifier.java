b5258c0fb68eea6caa6e11f0c405d9d80220b132
Ben Holland <benjholla@gmail.com>
2015-10-13 21:02:40 -0500

    adding the BCEL library with bytecode verification, but having some classpath problems

---
+++ TestMerge/src/verifier/ClassVerifier.java
@@ -0,0 +1,16 @@
+package verifier;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ClassVerifier {
+
+	public static void verify(String qualifiedClassName) throws IOException {
+		org.apache.bcel.verifier.Verifier.main(new String[]{qualifiedClassName});
+	}
+	
+	public static void main(String[] args){
+		org.apache.bcel.verifier.GraphicalVerifier.main(new String[]{});
+	}
+	
+}
