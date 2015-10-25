51c265f4dcf3e8af9d8a658f9e51a806569457c0
Ben Holland <benjholla@gmail.com>
2015-10-25 13:31:37 -0500

    Adding run/debug launch profile with tab group

---
+++ jreframeworker/src/jreframeworker/launcher/TabGroup.java
@@ -0,0 +1,30 @@
+package jreframeworker.launcher;
+
+import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
+import org.eclipse.debug.ui.CommonTab;
+import org.eclipse.debug.ui.ILaunchConfigurationDialog;
+import org.eclipse.debug.ui.ILaunchConfigurationTab;
+import org.eclipse.jdt.debug.ui.launchConfigurations.JavaArgumentsTab;
+import org.eclipse.jdt.debug.ui.launchConfigurations.JavaClasspathTab;
+import org.eclipse.jdt.debug.ui.launchConfigurations.JavaJRETab;
+import org.eclipse.jdt.debug.ui.launchConfigurations.JavaMainTab;
+
+public class TabGroup extends AbstractLaunchConfigurationTabGroup {
+	
+	/**
+	 * Creates the tabs contained in this tab group for the specified launch
+	 * mode.
+	 */
+	@Override
+	public void createTabs(ILaunchConfigurationDialog dialog, String mode) {
+		ILaunchConfigurationTab[] tabs = new ILaunchConfigurationTab[] {
+				new JavaMainTab(),
+				new JavaArgumentsTab(),
+				new JavaJRETab(),
+				new JavaClasspathTab(),
+				new CommonTab()
+		};
+		setTabs(tabs);
+	}
+
+}
