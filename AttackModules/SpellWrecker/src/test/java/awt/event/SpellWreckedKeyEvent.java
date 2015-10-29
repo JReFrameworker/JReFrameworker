6517da45648465660588a3b8a0c406d5b8a4ccff
Ben Holland <benjholla@gmail.com>
2015-10-29 14:41:24 -0500

    adding spellwrecker, need to fix monitor logic, right now spellwrecks are a 1 in 100 chance for typo.

---
+++ AttackModules/SpellWrecker/src/test/java/awt/event/SpellWreckedKeyEvent.java
@@ -0,0 +1,25 @@
+package test.java.awt.event;
+
+import java.awt.Frame;
+import java.awt.event.KeyEvent;
+
+import jreframeworker.annotations.methods.MergeMethod;
+import jreframeworker.annotations.types.MergeType;
+
+@MergeType
+public class SpellWreckedKeyEvent extends KeyEvent {
+
+	private static final long serialVersionUID = 1L;
+
+	public SpellWreckedKeyEvent(Frame frame, int i, long currentTimeMillis, int j, int vkUndefined, char c) {
+		super(frame, i, currentTimeMillis, j, vkUndefined, c);
+	}
+
+	@MergeMethod
+	@Override
+	public char getKeyChar(){
+		char original = super.getKeyChar();
+		return SpellWrecker.spellwreck(original);
+	}
+
+}
