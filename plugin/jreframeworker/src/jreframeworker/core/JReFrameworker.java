d0c5c56ae4a09cba297ac0e1b39d3b2d6f1dfe58
benjholla <benjholla@gmail.com>
2017-01-22 17:29:39 -0600

    added support for jvm standard libs (compatible jvms) and relative library paths

--- plugin/jreframeworker/src/jreframeworker/core/JReFrameworker.java
+++ plugin/jreframeworker/src/jreframeworker/core/JReFrameworker.java
@@ -3,6 +3,7 @@ package jreframeworker.core;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -42,9 +43,7 @@ import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
-import org.eclipse.jdt.launching.IVMInstall;
-import org.eclipse.jdt.launching.JavaRuntime;
-import org.eclipse.jdt.launching.LibraryLocation;
+import org.eclipse.jdt.internal.core.ClasspathEntry;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
@@ -67,6 +66,7 @@ public class JReFrameworker {
 	public static final String BINARY_DIRECTORY = "bin";
 	public static final String LIBRARY_DIRECTORY = "lib";
 	public static final String JRE_FRAMEWORKER_ANNOTATIONS_JAR = "JReFrameworkerAnnotations.jar";
+	public static final String ANNOTATIONS_JAR_PATH = ANNOTATIONS_DIRECTORY + "/" + JRE_FRAMEWORKER_ANNOTATIONS_JAR;
 	public static final String XML_BUILD_FILENAME = "jref-build.xml";
 	
 	public static LinkedList<IJavaProject> getJReFrameworkerProjects(){
@@ -183,15 +183,34 @@ public class JReFrameworker {
 			Log.warning("Could not created bin folder.", e);
 		}
 		
-		// add the runtime libraries
-		cloneDefaultRuntimeLibraries(jProject);
+		// create a set of classpath entries
+		List<IClasspathEntry> entries = new ArrayList<IClasspathEntry>(); 
 		
-		// create source folder
+		// adds classpath entry of: <classpathentry kind="con" path="org.eclipse.jdt.launching.JRE_CONTAINER/org.eclipse.jdt.internal.debug.ui.launcher.StandardVMType/JavaSE-1.8"/>
+		String path = org.eclipse.jdt.launching.JavaRuntime.JRE_CONTAINER + "/" + org.eclipse.jdt.internal.launching.StandardVMType.ID_STANDARD_VM_TYPE + "/" + "JavaSE-1.8";
+		entries.add(JavaCore.newContainerEntry(new Path(path)));
+		
+		//  add the jreframeworker annotations jar 
+		addProjectAnnotationsLibrary(jProject);
+
+		// have to create this manually instead of using JavaCore.newLibraryEntry because JavaCore insists the path be absolute
+		IClasspathEntry relativeAnnotationsLibraryEntry = new ClasspathEntry(IPackageFragmentRoot.K_BINARY,
+				IClasspathEntry.CPE_LIBRARY, new Path(ANNOTATIONS_JAR_PATH), ClasspathEntry.INCLUDE_ALL, // inclusion patterns
+				ClasspathEntry.EXCLUDE_NONE, // exclusion patterns
+				null, null, null, // specific output folder
+				false, // exported
+				ClasspathEntry.NO_ACCESS_RULES, false, // no access rules to combine
+				ClasspathEntry.NO_EXTRA_ATTRIBUTES);
+		entries.add(relativeAnnotationsLibraryEntry);
+		
+		// create source folder and add it to the classpath
 		IFolder sourceFolder = jProject.getProject().getFolder(SOURCE_DIRECTORY);
 		sourceFolder.create(false, true, null);
+		IPackageFragmentRoot sourceFolderRoot = jProject.getPackageFragmentRoot(sourceFolder);
+		entries.add(JavaCore.newSourceEntry(sourceFolderRoot.getPath()));
 		
-		// add source folder to project class entries
-		addClasspathEntry(jProject, sourceFolder);
+		// set the class path
+		jProject.setRawClasspath(entries.toArray(new IClasspathEntry[entries.size()]), null);
 		
 		Log.info("Successfully created JReFrameworker project [" + jProject.getProject().getName() + "]");
 	}
@@ -218,54 +237,56 @@ public class JReFrameworker {
 		return jProject;
 	}
 
-	private static void addClasspathEntry(IJavaProject jProject, IResource resource) throws JavaModelException {
-		IPackageFragmentRoot root = jProject.getPackageFragmentRoot(resource);
-		IClasspathEntry[] oldEntries = jProject.getRawClasspath();
-		IClasspathEntry[] newEntries = new IClasspathEntry[oldEntries.length + 1];
-		System.arraycopy(oldEntries, 0, newEntries, 0, oldEntries.length);
-		newEntries[oldEntries.length] = JavaCore.newSourceEntry(root.getPath());
-		jProject.setRawClasspath(newEntries, null);
-	}
+//	private static void addClasspathResourceEntry(IJavaProject jProject, IResource resource) throws JavaModelException {
+//		IPackageFragmentRoot root = jProject.getPackageFragmentRoot(resource);
+//		IClasspathEntry[] oldEntries = jProject.getRawClasspath();
+//		IClasspathEntry[] newEntries = new IClasspathEntry[oldEntries.length + 1];
+//		System.arraycopy(oldEntries, 0, newEntries, 0, oldEntries.length);
+//		newEntries[oldEntries.length] = JavaCore.newSourceEntry(root.getPath());
+//		jProject.setRawClasspath(newEntries, null);
+//	}
 	
-	private static void cloneDefaultRuntimeLibraries(IJavaProject jProject) throws IOException, JavaModelException, URISyntaxException {
-		
-		List<IClasspathEntry> entries = new ArrayList<IClasspathEntry>();
-		
-		// add the default JVM classpath (assuming translator uses the same jvm libraries)
-		IVMInstall vmInstall = JavaRuntime.getDefaultVMInstall();
-		LibraryLocation[] locations = JavaRuntime.getLibraryLocations(vmInstall);
-		for (LibraryLocation library : locations) {
-			entries.add(JavaCore.newLibraryEntry(library.getSystemLibraryPath(), null, null));
-		}
-		
-		// add the jreframeworker operations jar to project and the classpath
-		final String annotationsJarFilename = JRE_FRAMEWORKER_ANNOTATIONS_JAR;
-		String annotationsJarPath = ANNOTATIONS_DIRECTORY + "/" + annotationsJarFilename;
+//	/**
+//	 * Searches for the default virtual machine
+//	 * Note: this methods creates a project that is hardcoded to the path of the installed VM
+//	 * @param jProject
+//	 * @throws IOException
+//	 * @throws JavaModelException
+//	 * @throws URISyntaxException
+//	 */
+//	@SuppressWarnings("unused")
+//	private static void setAbsoluteProjectClasspath(IJavaProject jProject) throws IOException, JavaModelException, URISyntaxException {
+//		List<IClasspathEntry> entries = new ArrayList<IClasspathEntry>();
+//		
+//		// add the default JVM classpath (assuming translator uses the same jvm libraries)
+//		IVMInstall vmInstall = JavaRuntime.getDefaultVMInstall();
+//		LibraryLocation[] locations = JavaRuntime.getLibraryLocations(vmInstall);
+//		for (LibraryLocation library : locations) {
+//			entries.add(JavaCore.newLibraryEntry(library.getSystemLibraryPath(), null, null));
+//		}
+//		
+//		// add the jreframeworker operations jar to project and the classpath
+//		setAnnotationsClasspath(jProject, entries);
+//		
+//		// set the class path
+//		jProject.setRawClasspath(entries.toArray(new IClasspathEntry[entries.size()]), null);
+//	}
+
+	private static File addProjectAnnotationsLibrary(IJavaProject jProject) throws IOException, URISyntaxException, MalformedURLException {
 		// see http://stackoverflow.com/q/23825933/475329 for logic of getting bundle resource
-		URL fileURL = Activator.getDefault().getBundle().getEntry(annotationsJarPath);
+		URL fileURL = Activator.getDefault().getBundle().getEntry(ANNOTATIONS_JAR_PATH);
 		URL resolvedFileURL = FileLocator.toFileURL(fileURL);
 		// need to use the 3-arg constructor of URI in order to properly escape file system chars
 		URI resolvedURI = new URI(resolvedFileURL.getProtocol(), resolvedFileURL.getPath(), null);
 		InputStream annotationsJarInputStream = resolvedURI.toURL().openConnection().getInputStream();
 		if(annotationsJarInputStream == null){
-			throw new RuntimeException("Could not locate: " + annotationsJarPath);
+			throw new RuntimeException("Could not locate: " + ANNOTATIONS_JAR_PATH);
 		}
 		File annotationsLibDirectory = new File(jProject.getProject().getLocation().toFile().getCanonicalPath() + File.separatorChar + ANNOTATIONS_DIRECTORY);
 		annotationsLibDirectory.mkdirs();
 		File annotationsJar = new File(annotationsLibDirectory.getCanonicalPath() + File.separatorChar + JRE_FRAMEWORKER_ANNOTATIONS_JAR);
 		Files.copy(annotationsJarInputStream, annotationsJar.toPath());
-		
-		// add the project libraries to the project classpath
-		// TODO: investigate, is this relative path getting computed correctly? path appeared to be absolute in some tests
-		String annotationsJarCanonicalPath = annotationsJar.getCanonicalPath();
-		String projectCanonicalPath = jProject.getProject().getLocation().toFile().getCanonicalPath();
-		String annotationsJarBasePath = annotationsJarCanonicalPath.substring(annotationsJarCanonicalPath.indexOf(projectCanonicalPath));
-		String annotationsJarParentCanonicalPath = annotationsJar.getCanonicalPath();
-		String annotationsJarParentBasePath = annotationsJarParentCanonicalPath.substring(annotationsJarParentCanonicalPath.indexOf(projectCanonicalPath));
-		entries.add(JavaCore.newLibraryEntry(new Path(annotationsJarBasePath), null, new Path(annotationsJarParentBasePath)));
-		
-		// set the class path
-		jProject.setRawClasspath(entries.toArray(new IClasspathEntry[entries.size()]), null);
+		return annotationsJar;
 	}
 
 	private static URI getProjectLocation(String projectName, IPath projectPath) {
