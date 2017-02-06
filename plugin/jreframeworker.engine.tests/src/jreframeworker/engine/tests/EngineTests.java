b64732ee280de9399adcf4208d0f3adc607098a0
Ben Holland <benjholla@gmail.com>
2017-02-05 20:24:50 -0600

    2 working and passing regression tests!

--- plugin/jreframeworker.engine.tests/src/jreframeworker/engine/tests/EngineTests.java
+++ plugin/jreframeworker.engine.tests/src/jreframeworker/engine/tests/EngineTests.java
@@ -1,87 +1,29 @@
 package jreframeworker.engine.tests;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.charset.Charset;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.nio.file.Files;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Locale;
-
-import javax.tools.Diagnostic;
-import javax.tools.DiagnosticCollector;
-import javax.tools.JavaCompiler;
-import javax.tools.JavaFileObject;
-import javax.tools.StandardJavaFileManager;
-import javax.tools.ToolProvider;
+import java.util.jar.JarException;
 
 import org.junit.Test;
 
 import jreframeworker.engine.Engine;
+import jreframeworker.engine.tests.utils.TestUtilities;
+import jreframeworker.engine.utils.BytecodeUtils;
 import junit.framework.TestCase;
 
 public class EngineTests extends TestCase {
 
-	private String projectSource = new File("src" + File.separator + "jreframeworker" + File.separator + "engine" + File.separator + "tests").getAbsolutePath();
+	private String packagePrefix = "jreframeworker.engine.tests";
+	private String projectSource = new File("src" + File.separator + packagePrefix.replace(".", File.separator)).getAbsolutePath();
 	private File workingDirectory = null;
-	
-	private void delete(File f) throws IOException {
-		if (f.isDirectory()){
-			for (File c : f.listFiles()){
-				delete(c);
-			}
-		}
-		if (!f.delete()){
-			throw new FileNotFoundException("Failed to delete file: " + f);
-		}
-	}
-	
-	private List<File> gatherTestSources(File testSourceDirectory){
-		ArrayList<File> sourceFiles = new ArrayList<File>();
-		for(File sourceFile : testSourceDirectory.listFiles()){
-			if(sourceFile.getName().endsWith(".java")){
-				sourceFiles.add(sourceFile);
-			}
-		}
-		return sourceFiles;
-	}
 
-	private List<File> compileSources(List<File> sourceFiles) throws IOException {
-		JavaCompiler javaCompiler = ToolProvider.getSystemJavaCompiler();
-		
-		if(javaCompiler == null){
-			throw new RuntimeException("Could not find Java compiler.");
-		}
-		
-		DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<JavaFileObject>();
-		StandardJavaFileManager fileManager = javaCompiler.getStandardFileManager(diagnostics, Locale.ENGLISH, Charset.forName("UTF-8"));
-		Iterable<? extends JavaFileObject> compilationUnits = fileManager.getJavaFileObjectsFromFiles(sourceFiles);
-		javaCompiler.getTask(null, fileManager, diagnostics, null, null, compilationUnits).call();
-		    
-		for (Diagnostic<?> diagnostic : diagnostics.getDiagnostics()) {
-			throw new RuntimeException(String.format("Error on line %d in %d%n", diagnostic.getLineNumber(), diagnostic.getSource().toString()));
-		}        
-		 
-		fileManager.close();
-		
-		List<File> classFiles = new LinkedList<File>();
-		for(File sourceFile : sourceFiles){
-			classFiles.add(new File(workingDirectory.getAbsolutePath() + File.separator + sourceFile.getName().replace(".java", ".class")));
-		}
-		return classFiles;
-	}
-	
-	public File getClassFile(String className, List<File> classFiles){
-		for(File classFile : classFiles){
-			if(classFile.getName().endsWith(className + ".class")){
-				return classFile;
-			}
-		}
-		return null;
-	}
-	
 	@Override
 	protected void setUp() throws Exception {
 		workingDirectory = Files.createTempDirectory("working-directory").toFile();
@@ -89,41 +31,92 @@ public class EngineTests extends TestCase {
 
 	@Override
 	protected void tearDown() throws Exception {
-		delete(workingDirectory);
+		TestUtilities.delete(workingDirectory);
 		workingDirectory = null;
 	}
 	
 	@Test
-	public void testMergeMethodReplacesOriginalMethod() {
+	public void testMergeMethodReplacesOriginalMethod() throws Exception {
 		// gather sources
-		File testSourceDirectory = new File(projectSource + File.separator + "inputs" + File.separator + "a");
-		List<File> sourceFiles = gatherTestSources(testSourceDirectory);
+		String pkg = "inputs.a";
+		File testSourceDirectory = new File(projectSource + File.separator + pkg.replace(".", File.separator));
+		List<File> sourceFiles = TestUtilities.gatherTestSources(testSourceDirectory);
 		
 		// compile sources
-		List<File> classFiles = new LinkedList<File>();
-		try {
-			classFiles.addAll(compileSources(sourceFiles));
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
+		List<File> classFiles = TestUtilities.compileSources(sourceFiles, workingDirectory);
 		
-		File baseClass = getClassFile("BaseClass", classFiles);
+		// assert all class files were compiled successfully
+		File baseClass = TestUtilities.getClassFile("BaseClass", classFiles);
 		assertNotNull(baseClass);
-		
-		File mergeClass = getClassFile("MergeClass", classFiles);
+		assertTrue(baseClass.exists());
+		File mergeClass = TestUtilities.getClassFile("MergeClass", classFiles);
 		assertNotNull(mergeClass);
+		assertTrue(mergeClass.exists());
 		
-		// TODO: jar base class
+		// jar base class
+		File originalJar = new File(workingDirectory.getAbsolutePath() + File.separator + "original.jar");
+		TestUtilities.jarFiles(workingDirectory, originalJar, (packagePrefix + "." + pkg), baseClass);
 		
-		// TODO: merge class into base class
-//		Engine engine = new Engine();
+		// merge class into base class
+		String renamePrefix = "jref_";
+		Engine engine = new Engine(originalJar, renamePrefix);
+		File modifiedJar = new File(workingDirectory.getAbsolutePath() + File.separator + "modified.jar");
+		engine.process(BytecodeUtils.writeClass(BytecodeUtils.getClassNode(mergeClass)));
+		engine.save(modifiedJar);
 		
-		fail("Not yet implemented");
+		// execute the modified base class method
+		URL[] jarURL = { new URL("jar:file:" + modifiedJar.getCanonicalPath() + "!/") };
+		ClassLoader classLoader = new URLClassLoader(jarURL, null); // important: set parent class loader to null!
+		Class modifiedBaseClass = classLoader.loadClass(packagePrefix + "." + pkg + "." + "BaseClass");
+		Method modifiedBaseClassMethod = modifiedBaseClass.getDeclaredMethod("method");
+		Object modifiedBaseClassInstance = modifiedBaseClass.newInstance();
+		Object result = modifiedBaseClassMethod.invoke(modifiedBaseClassInstance);	
+		
+		// assert expected behavior is observed
+		// the replaced method should return merge-method
+		assertEquals("merge-method", result);
 	}
 	
 	@Test
-	public void testMergeMethodPreservesOriginalMethod() {
-		fail("Not yet implemented");
+	public void testMergeMethodPreservesOriginalMethod() throws Exception {
+		// gather sources
+		String pkg = "inputs.b";
+		File testSourceDirectory = new File(projectSource + File.separator + pkg.replace(".", File.separator));
+		List<File> sourceFiles = TestUtilities.gatherTestSources(testSourceDirectory);
+		
+		// compile sources
+		List<File> classFiles = TestUtilities.compileSources(sourceFiles, workingDirectory);
+		
+		// assert all class files were compiled successfully
+		File baseClass = TestUtilities.getClassFile("BaseClass", classFiles);
+		assertNotNull(baseClass);
+		assertTrue(baseClass.exists());
+		File mergeClass = TestUtilities.getClassFile("MergeClass", classFiles);
+		assertNotNull(mergeClass);
+		assertTrue(mergeClass.exists());
+		
+		// jar base class
+		File originalJar = new File(workingDirectory.getAbsolutePath() + File.separator + "original.jar");
+		TestUtilities.jarFiles(workingDirectory, originalJar, (packagePrefix + "." + pkg), baseClass);
+		
+		// merge class into base class
+		String renamePrefix = "jref_";
+		Engine engine = new Engine(originalJar, renamePrefix);
+		File modifiedJar = new File(workingDirectory.getAbsolutePath() + File.separator + "modified.jar");
+		engine.process(BytecodeUtils.writeClass(BytecodeUtils.getClassNode(mergeClass)));
+		engine.save(modifiedJar);
+		
+		// execute the modified base class method
+		URL[] jarURL = { new URL("jar:file:" + modifiedJar.getCanonicalPath() + "!/") };
+		ClassLoader classLoader = new URLClassLoader(jarURL, null); // important: set parent class loader to null!
+		Class modifiedBaseClass = classLoader.loadClass(packagePrefix + "." + pkg + "." + "BaseClass");
+		Method modifiedBaseClassMethod = modifiedBaseClass.getDeclaredMethod("method");
+		Object modifiedBaseClassInstance = modifiedBaseClass.newInstance();
+		Object result = modifiedBaseClassMethod.invoke(modifiedBaseClassInstance);	
+		
+		// assert expected behavior is observed
+		// the replaced method should return merge-method
+		assertEquals("merged-original-method", result);
 	}
 
 }
