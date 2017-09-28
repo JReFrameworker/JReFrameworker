d2fa69a40e81bcb0c0f30fc7a74cbedbcfba4ced
benjholla <benjholla@gmail.com>
2017-09-28 13:47:16 -0500

    Code cleanup

--- plugin/jreframeworker.atlas/src/jreframeworker/atlas/ui/CodeWizardView.java
+++ plugin/jreframeworker.atlas/src/jreframeworker/atlas/ui/CodeWizardView.java
@@ -1,19 +1,18 @@
 package jreframeworker.atlas.ui;
 
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.ui.part.ViewPart;
-import org.eclipse.swt.custom.SashForm;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.widgets.Group;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Text;
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.custom.SashForm;
 import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.part.ViewPart;
 
 public class CodeWizardView extends ViewPart {
 	
